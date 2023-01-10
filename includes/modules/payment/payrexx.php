08d98077f9697a820d532ec99a38bbdb9f931381
vinothganesan <vinoth@payrexx.com>
2023-01-10 19:31:23 +0530

    added payment method

---
+++ includes/modules/payment/payrexx.php
@@ -0,0 +1,27 @@
+<?php
+/* --------------------------------------------
+ * Class payrexx_ORIGIN.
+ *
+ * Payment gateway for Payrexx AG.
+ *
+ * @category   Payment Module
+ *
+ * @link https://www.payrexx.com
+ *
+ * @author Payrexx <support@payrexx.com>
+ *
+ * @copyright  2023 Payrexx
+ *
+ * @license MIT License
+ *
+* VERSION HISTORY:
+* 1.0.0 Payrexx Payment Gateway.
+---------      -----------------------------*/
+
+class payrexx_ORIGIN extends PayrexxPaymentGatewayBase
+{
+    public $code = 'payrexx';
+    public $sort_order = 9999;
+}
+
+MainFactory::load_origin_class('payrexx');
