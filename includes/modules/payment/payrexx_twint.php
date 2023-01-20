de7b934b333556577496cb37606f34cc83c1f8e5
vinothganesan <vinoth@payrexx.com>
2023-01-20 19:20:49 +0530

    task/PP-8959: extend functionality to support payment method

---
+++ includes/modules/payment/payrexx_twint.php
@@ -0,0 +1,26 @@
+<?php
+/* --------------------------------------------
+ * Class payrexx_twint_ORIGIN.
+ *
+ * Payment gateway for Payrexx AG.
+ *
+ * @category   Payment Module
+ *
+ * @link https://www.payrexx.com
+ *
+ * @author Payrexx <support@payrexx.com>
+ *
+ * @copyright  2023 Payrexx
+ *
+ * @license MIT License
+ *
+* VERSION HISTORY:
+* 1.0.0 Payrexx Payment Gateway.
+---------      -----------------------------*/
+
+class payrexx_twint_ORIGIN extends PayrexxPaymentGatewayBase
+{
+    public $code = 'payrexx_twint';
+}
+
+MainFactory::load_origin_class('payrexx_twint');
