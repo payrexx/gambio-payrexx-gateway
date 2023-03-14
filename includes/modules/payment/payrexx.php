56595b3c768917cbbd6216225e9c34b5c337cc9a
vinothganesan <vinoth@payrexx.com>
2023-03-14 14:20:22 +0530

    add doc block

--- includes/modules/payment/payrexx.php
+++ includes/modules/payment/payrexx.php
@@ -1,17 +1,30 @@
 <?php
 /**
- * Class payrexx_ORIGIN.
+ * Class payrexx_ORIGIN
  *
  * Payment gateway for Payrexx AG.
  *
- * @category  Payment Module
- * @link      https://www.payrexx.com
+ * PHP version 7,8
+ *
+ * @category  PaymentModule
+ * @package   PayrexxPayemntGateway
  * @author    Payrexx <integration@payrexx.com>
  * @copyright 2023 Payrexx
  * @license   MIT License
+ * @link      https://www.payrexx.com
  *
  * VERSION HISTORY:
  * 1.0.0 Payrexx Payment Gateway.
+ */
+
+/**
+ * Class payrexx_ORIGIN
+ *
+ * @category PaymentModule
+ * @package  PayrexxPayemntGateway
+ * @author   Payrexx <integration@payrexx.com>
+ * @license  MIT License
+ * @link     https://www.payrexx.com
  */
 class payrexx_ORIGIN extends PayrexxPaymentGatewayBase
 {
