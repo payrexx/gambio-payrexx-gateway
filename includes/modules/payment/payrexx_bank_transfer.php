261eab667f3089171e4b03acb602a49daacfbd1b
vinoth <vinoth@payrexx.com>
2024-03-18 14:08:20 +0530

    bugfix/PP-11107: payment method bank transfer

---
+++ includes/modules/payment/payrexx_bank_transfer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Class payrexx_bank_transfer_ORIGIN
+ *
+ * Payment gateway for Payrexx AG.
+ *
+ * PHP version 7,8
+ *
+ * @category  PaymentModule
+ * @package   PayrexxPayemntGateway
+ * @author    Payrexx <integration@payrexx.com>
+ * @copyright 2024 Payrexx
+ * @license   MIT License
+ * @link      https://www.payrexx.com
+ *
+ * VERSION HISTORY:
+ * 1.0.0 Payrexx Payment Gateway.
+ */
+
+/**
+ * Class payrexx_bank_transfer_ORIGIN
+ *
+ * @category PaymentModule
+ * @package  PayrexxPayemntGateway
+ * @author   Payrexx <integration@payrexx.com>
+ * @license  MIT License
+ * @link     https://www.payrexx.com
+ */
+class payrexx_bank_transfer_ORIGIN extends PayrexxPaymentGatewayBase
+{
+    public $code = 'payrexx_bank_transfer';
+}
+
+MainFactory::load_origin_class('payrexx_bank_transfer');
