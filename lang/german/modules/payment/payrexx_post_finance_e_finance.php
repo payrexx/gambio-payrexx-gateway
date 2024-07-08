fc8953d112622f94224317f10c8d513af70479a0
vinoth <vinoth@payrexx.com>
2024-07-08 13:48:26 +0530

    task/PP-12161: Removed post finance card and efinance, Added postfinance pay.

--- lang/german/modules/payment/payrexx_post_finance_e_finance.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (!defined('MODULE_PAYMENT_PAYREXX_POST_FINANCE_E_FINANCE_TEXT_TITLE')) {
-    define('MODULE_PAYMENT_PAYREXX_POST_FINANCE_E_FINANCE_TEXT_TITLE', 'Payrexx Post Finance E Finance');
-}
